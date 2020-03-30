f6c69e62fad1e25c9a453c63f91785c6f53f30c7
lenaonisch <elenaonishhenk0@gmail.com>
2020-03-30 08:38:11 +0300

    commit

---
+++ WoodpeckerBot/EchoBot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using Telegram.Bot.Framework;
+using Telegram.Bot.Types;
+
+namespace Quickstart.AspNetCore
+{
+    public class EchoBot : BotBase
+    {
+        public EchoBot(IOptions<BotOptions<EchoBot>> options)
+            : base(options.Value)
+        {
+        }
+    }
+}
